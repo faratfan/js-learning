efc5800993215deeab25b4ff72c2f467ab4b21ae
faratfan <164708808+faratfan@users.noreply.github.com>
2024-03-28 00:15:00 +0000

    done

--- .devcontainer/01_basic/03_conversrionOperation.js
+++ .devcontainer/01_basic/03_conversrionOperation.js
@@ -1,2 +1,9 @@
-let score = 33;
+ let score = 33;//here we know the type
 console.log(typeof(score));
+console.log(typeof score);
+// some time we are working at backend so we don'r always know the type of data e.g where score have came from
+//const {score}=req.body// we are not sure where data came from
+const scores = "33";
+console.log(typeof(scores));
+const valueInNumber = Number(score);
+console.log(typeof valueInNumber);
