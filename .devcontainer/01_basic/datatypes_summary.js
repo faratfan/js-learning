1b2361e00f292e4b196d86097e0bec63d922d701
faratfan <164708808+faratfan@users.noreply.github.com>
2024-03-31 15:21:52 +0000

    summary done

---
+++ .devcontainer/01_basic/datatypes_summary.js
@@ -0,0 +1,16 @@
+//DataTypes are converted into two categories primitive and nonPrimitive/refrence type
+//How data is assigne , int , boolean, number ...
+//How data is access(--call by value and refrence) ...
+// primitive<-- all are call by value(while using them we are give a copy of primitive type but not the orignal memory location of it)
+// 7 basic  strings, boolena, Numbers,null,undefined,symbol(to make a value unique),BigInt
+//number-->    ---> JavaScript is a Dynamically typed  language.
+const score = 100;
+const score1 = 10.3;
+const say = " How are ya?";
+const isLogged= true;
+const temp = null;
+const email; --. undefined
+
+//refrence type/non - primitive type<-- refrence of them it directly allocated;
+//Array , Objects, Functions
+//Typescript is another similar language to java script 
