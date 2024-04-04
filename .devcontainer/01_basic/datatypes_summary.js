da5c7f4a318e76d87aa0847dbaf5e65487f7ed5e
faratfan <164708808+faratfan@users.noreply.github.com>
2024-04-04 01:47:08 +0000

    summary done

--- .devcontainer/01_basic/datatypes_summary.js
+++ .devcontainer/01_basic/datatypes_summary.js
@@ -4,13 +4,38 @@
 // primitive<-- all are call by value(while using them we are give a copy of primitive type but not the orignal memory location of it)
 // 7 basic  strings, boolena, Numbers,null,undefined,symbol(to make a value unique),BigInt
 //number-->    ---> JavaScript is a Dynamically typed  language.
-const score = 100;
-const score1 = 10.3;
-const say = " How are ya?";
-const isLogged= true;
-const temp = null;
-const email; --. undefined
+//const score = 100;
+// const score1 = 10.3;
+// const say = " How are ya?";
+// const isLogged= true;
+// const temp = null;
+// const email = "1";// undefined
+// const id = Symbol('123');
+// const anotherId = Symbol('123');
+// console.log(id === anotherId);
+// const bigNumber = 184652268416n;//putting n at the end;
 
 //refrence type/non - primitive type<-- refrence of them it directly allocated;
 //Array , Objects, Functions
 //Typescript is another similar language to java script 
+
+const heros =['SuperMan','GreenArrow','WonderWoman','AuqaMan'];
+// let myObj = {
+//     name :fann,
+//     work :Software Developer,
+
+// }
+//we can treat function as another variable in javascript;
+// let  marvel = function villians(vil) {
+//     const arr= ['Thanos','RedSkull','Doctor'];
+//          arr.push(vil);
+// }
+//  console.log(marvel('Loki'));
+// let myFunc = function antiHero (a) {
+//     let array = ['Thanos','RedSkull','Doctor'];
+//     array.push(a);
+//     return array;
+// }
+
+// antiHero('Loki')
+// console.log(myFunc);
