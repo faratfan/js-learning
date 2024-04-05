a1f0e35dc545d717ed0ab7867adba3cd0827930c
faratfan <164708808+faratfan@users.noreply.github.com>
2024-04-05 02:02:31 +0000

    changes done

---
+++ .devcontainer/01_basic/05_strings.js
@@ -0,0 +1,5 @@
+const name = "Fann";
+const repoCount = 2;
+//console.log(name + repoCount);//older way--> string concatenation
+//the other way is string Interpolation-->${} creating palce holder with ``backticks
+console.log(`Hello my name is ${name} and my GitHub repo count is ${repoCount}`);
