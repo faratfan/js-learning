821b1a39e3055ecfadae32aeb77db7bd08662d75
faratfan <164708808+faratfan@users.noreply.github.com>
2024-04-11 19:46:11 +0000

    commit

---
+++ .devcontainer/01_basic/06_Nums_Maths.js
@@ -0,0 +1,25 @@
+const score = 400;
+console.log(score)
+//Here javascript detect its a number.here score 
+//const balance = new Number(100)
+//console.log(balance) 
+//above there is no wondering of type of number.the output here is [Number:100]
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+//to.fixed is mostly used to calculate number of fixed to get precison value(used mostly in ecommerce)
+
+//const otherNumber = 23.8966;//as we applied the precision  to 3 digits the output will be 23.9
+//const otherNumber = 123.8966; //expected answer 124
+//const otherNumber = 1123.8966;//1.12 other value in exponent form so 1.12e+3
+//console.log(otherNumber.toPrecision(3));
+//const otherNumber = 123.8966;
+//console.log(otherNumber.toPrecision(4));
+//##### using locale string
+//const hundreds = 1000000;
+//console.log(hundreds.toLocaleString('en-IN'));
+//Max ansd minvalue
+//#####Maths /Library######
+consoe
+
+
+
