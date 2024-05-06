da4a4d9e9f79b4c56a884ec6176348aaee8a41e8
faratfan <164708808+faratfan@users.noreply.github.com>
2024-05-06 23:38:37 +0000

    arrow

--- .devcontainer/03_basic/03_arrow.js
+++ .devcontainer/03_basic/03_arrow.js
@@ -1,2 +1,17 @@
-console.log("Lets get going");
-console.log("Check agian");
+//console.log("Lets get going");
+//console.log("Check agian");
+const user ={
+    username: "Fann",
+    cost : 999,
+ //Use `this` to represnt current context; 
+    greeting : function (){
+        console.log (`hey ${this.username}, the price is ${this.cost} `);
+    }
+
+}
+console.log(user.greeting(user))
+//Checking arrow function
+const add = (n) => {
+   return n;
+}
+console.log(add(4));
