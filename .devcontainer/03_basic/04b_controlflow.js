8265e3e0159a0dfd583abadcd3cee65c57810634
faratfan <164708808+faratfan@users.noreply.github.com>
2024-05-14 22:12:22 +0000

    control flow

--- .devcontainer/03_basic/04b_controlflow.js
+++ .devcontainer/03_basic/04b_controlflow.js
@@ -68,11 +68,128 @@ if (score > 100){
 
  const score2 = '300';
  
- if ( result) {
+ if ( score2) {
      var level = 2;
-     console.log(`you reached level, ${level}`);
+      console.log(`you reached level, ${level}`);
+ }
+ console.log(`Your can move to level, ${level}`)// inside block scope var could  be access
+ //note : the better practice would be to not use var inside scope and use const and let instead
+// short hand notation--> using 'implecit scope';
+const balance = 1000;
+//it is better to avoide implicite block scope
+//if(balance > 500) console.log("imolicite block 1"),
+//console.log("implicit block 2");
+// nested contionals
+if (balance < 500){
+    console.log("The balance is  less than 500")
+}else if (balance < 750) {
+    console.log('The balance is less than 750')
+}else {
+    console.log('The balanc is 1000')
+}
+//use case of 
+const userLoggedIn1 = true;
+const debitCard = true;
+const loggedInFromGoogle  = false;
+const loggedInFromEmail = true;
+
+if( userLoggedIn1 && debitCard){
+    console.log('thank you for your purchase')
+}
+if (loggedInFromGoogle || loggedInFromEmail){
+    console.log('User Logged In ')
+}
+//Swith case syntax
+//swith (key){
+//    case value:
+
+//    break;
+
+//    default 
+        // break;
+//}
+//Using Exampel
+const month = 3;
+
+switch (month){
+    case 1:
+        console.log("Janauary");
+        break;
+    case 2:
+        console.log("Feburary ");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    default :
+        console.log("default")
+         break;
+}
+//Note break helps to ctrol flow of switch statement, use an example to understant;
+ const password = 4;
+
+ switch(password){
+    case 1: 
+    console.log("Password is 1");
+    break;
+    case 2: 
+    console.log("Password is 2");
+    break;
+    case 3: 
+    console.log("Password is 3");
+    break;
+    case 4: 
+    console.log("Password is 4");
+    //break; Now the code will not break and run all the values except default
+    case 5: 
+    console.log("Password is 5");
+    break;
+    case 6: 
+    console.log("Password is 6");
+    break;
+    default:
+        console.log("this is defaut");
+    break;
  }
 
+//Truthy and falsy value
+//fasy valu
+//false,0,-0,bigInt 0n,"",Null,undefine
+//truthy value 
+//"0",'false'," ",[]empty arry, function () {} empty function,
+//To check if an array is empty 
 
+const numbers= [];// take array name check it against its elements  
 
+if(numbers.length === 0){
+    console.log("Array is Empty")
+}
+
+//To find if an object is empty
+const emptyObject= {};
+//using Object.key for output in array
+if(Object.keys(emptyObject).length === 0){
+    console.log("Object is Empty")
+}
+//interview Question
+//false == 0, true
+//false == ' ', true
+//0 == '',true;
+
+//Nullish Coalescing Operator(??) Null and undefine 
+let val1 ;
+//val1= 5 ?? 10;
+//val1 = null ?? 10
+//var1 = undefined ?? 10; 
+
+val1= null ?? 10 ?? 20
+
+console.log(val1);
+
+//Terniary Operator
+//Syntax
+//
+//condition ? true : false
+const iceTeaPrice =100;
+iceTeaPrice  <= 80 ? console.log("price is less than 80") : console.log("Price is greather than 80");
 
