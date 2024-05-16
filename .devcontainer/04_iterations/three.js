73f580199ed6db96bb355845f3928a7ff1b3cf9c
faratfan <164708808+faratfan@users.noreply.github.com>
2024-05-16 23:39:24 +0000

    maps

---
+++ .devcontainer/04_iterations/three.js
@@ -0,0 +1,22 @@
+console.log("High Order Array Loop");
+//for of
+//string with Array
+["","",""]
+//Object within Array
+[{},{},{}]
+//loop>object>.notion/[]notion
+const arr = [1,2,3,4,5]
+//loop for object
+//--> arr
+for (const num of arr) {
+    //console.log(num);
+}
+//
+const greetings = "Hello World";
+
+for (const greet of greetings) {
+    console.log(`Each Character Is : ${greet}`);
+}
+//Maps --> 
+const map = new Map()
+map.
