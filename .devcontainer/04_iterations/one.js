43e009967de10006a4f5a5605ae0e27ea62a0201
faratfan <164708808+faratfan@users.noreply.github.com>
2024-05-16 23:11:22 +0000

    two

---
+++ .devcontainer/04_iterations/one.js
@@ -0,0 +1,62 @@
+console.log("Iterations") 
+
+for(let i = 0 ; i <= 10; i++ ){
+    console.log(i)
+}
+const array = [1,2,3,4,5,6];
+/*for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    console.log(element);
+    
+}*/
+for (let i = 0; i <= array.length; i++) {
+    const element = array[i];
+    if(i == 5){
+        console.log('5 is the best number');
+    }
+    console.log(element);
+}
+console.log(array);
+//console.log(element)
+const names = ["puffy","April","Jaggi","Luna","Ozzy","Jalebi"]
+ for( let  i = 0 ; i <= names.length ; i++){
+    const result = names[i]
+    console.log(result);
+ } 
+//Nested fr loop
+ /*for (let i = 0; i <= 10; i++) {
+    //outerloop
+    //console.log(`This is outer loop: ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        //innerlop
+        //console.log(`This is inner loop ${j} , this is outer loop: ${i}`);
+        console.log(i + 'x' + j +'=' + i*j );
+    }
+    
+ }*/
+  //Workign with aray and for loop
+
+ const array2 = ['april', 'puffy', 'Luna'];
+  for (let i = 0; i < array2.length; i++) {
+       const element = array2[i];
+    console.log(element)
+    
+  }
+  //how much of the loop  to print
+  //break and continue
+  for (let i = 1 ; i <= 20; i++) {
+    //const element = array[i];
+
+    if(i == 5){
+        console.log('5  is  Detected')
+        console.log('5 is the lukiest number');
+        //break// after break the code will not execte any  further;
+        //
+        //continue--> to kep going -->
+    }
+     console.log(`value of i is: ${i}`)
+    
+  }
+    
+  
+        
---
+++ .devcontainer/04_iterations/two.js
@@ -0,0 +1,30 @@
+console.log("do-While loop")
+//Syntax
+
+/*while(condition) {
+    console.log();
+    exit.
+}*/
+//let index = 0 ;
+//while( index  <= 10) {
+    //console.log(`The value of index is : ${index}`)
+    //index++// to exit this loop
+    //index = index + 2 ;
+    
+//}
+let myArray = ['Puffy','April','Luna'];
+
+let arr= 0;
+
+while (arr < myArray.length) {
+    console.log(`baby is ${myArray[arr]}`);
+    arr++;
+    // arr = arr+1;
+}
+//do-While()
+let score = 11;
+do{
+    console.log(`score is ${score}`);
+    score ++;//exit
+}while(score <= 10);
+//loop for arrays
