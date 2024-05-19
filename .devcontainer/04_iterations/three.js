bac7567d7ec6e2ce55c25b8ec1437d7b88ef2652
faratfan <164708808+faratfan@users.noreply.github.com>
2024-05-19 19:54:15 +0000

    three

--- .devcontainer/04_iterations/three.js
+++ .devcontainer/04_iterations/three.js
@@ -15,8 +15,64 @@ for (const num of arr) {
 const greetings = "Hello World";
 
 for (const greet of greetings) {
-    console.log(`Each Character Is : ${greet}`);
+    //console.log(`Each Character Is : ${greet}`);
 }
-//Maps --> 
+//Maps --> store unique value and order is maintaien
 const map = new Map()
-map.
+map.set('IN', "India");
+map.set('USA',"United States of America");
+map.set('ENG',"England");
+map.set('LN',"London");
+map.set('IN',"INDIA");//The line wont print as map only store unique value
+//console.log(map)
+//using loop on map(for of)
+for (const index of map) {
+    //console.log(`The values are ${index}`);
+}
+//
+for (const key of map) {
+    //console.log(`The key is  :- ${key}`);
+}
+//key-value,De-Structure Array
+for (const [key,value] of map) {
+   // console.log(`The key ${key}, value ${value}`);
+}
+//Practice
+const map2 = new Map()
+    //Capital city 
+    map2.set('LN', "London");
+    map2.set('PR',"Paris");
+    map2.set('DC',"Washington");
+
+    //console.log(map2);
+  // using loop For of on map2
+   for (const index of map2) {
+    //console.log(`The Values are : ${index}`);
+   }
+   for (const [key , value] of  map2) {
+      //console.log(`The key ${key} and the value ${value}`);
+      //console.log(key,':-',value);
+   }
+//Practice 2
+const map3 = new Map()
+map3.set('1',"Object")
+map3.set('2',"Array")
+map3.set('3',"Function")
+
+//console.log(map3);
+for (const index of map3) {
+    //console.log(`The order is : ${index}`);
+}
+//De structure Array
+for (const [key,value] of map3) {
+    //console.log(` the key and value is ${key,':-', value}`);
+}
+//Using For Of Loop on Object(will not work)
+const myObject ={
+    game1 : 'Call of Duty ',
+    game2 : 'NFS',
+    game3 : 'Assasin Creed',
+}
+for (const [key,value] of myObject) {
+    console.log(key,':-',value);//myObject is not iteratable
+}
